54f698cceee92fb9b6c93b5f0ccc0136c9b8b4c5
ronin540 <satyamor2@gmail.com>
2021-10-09 20:30:54 +0530

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/sendmail", async (req, res) => {
 	try {
